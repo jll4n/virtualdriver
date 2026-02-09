5740e344c32d50ed27e56e5b5385b2db9633a5a5
jll4n <julianpoulainpro@gmail.com>
2026-02-09 21:45:42 +0000

    netinterface v1.0.0

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <linux/module.h>
 #include <linux/init.h>
+#include <linux/netdevice.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("jll4n");
@@ -13,6 +14,12 @@ static int __init virtualdriver_init(void){
 static void __exit virtualdriver_exit(void){
     printk(KERN_INFO "virtualdriver exit\n");
 }
+
+int virtualdriver_open(struct net_device *dev){ //a test 
+    printk(KERN_INFO "virtualdriver open\n");
+    return 0;
+}
+// struct net_device *virtual_netdev;
 
 module_init(virtualdriver_init);
 module_exit(virtualdriver_exit);  
